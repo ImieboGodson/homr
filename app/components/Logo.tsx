741fe0895aec097bc39b669fef7e907ca3d755aa
Godson Imiebo <imiebo.godson@gmail.com>
2023-09-26 13:52:49 +0100

    feat: logo homeUrl when user is signed in

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,13 +1,20 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { SafeUser } from "../types";
 
-const Logo = () => {
+interface LogoProps {
+  currentUser?: SafeUser | null;
+}
+
+const Logo: React.FC<LogoProps> = ({ currentUser }) => {
   const router = useRouter();
+  const homeUrl = currentUser ? "/listings" : "/";
+
   return (
     <div
-      onClick={() => router.push("/")}
+      onClick={() => router.push(homeUrl)}
       className="flex flex-row items-end cursor-pointer"
     >
       <Image src="/images/logo.png" alt="logo" height={35} width={35} />
